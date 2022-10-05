ec9b41a86f848d9618e99cc904dbd207ce045f42
Johnson <johnson@github.com>
2022-10-04 21:45:05 -0300

    Adicionado react-router-dom.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 export const App = () => {
- 
   return (
-    <div>
-     <h1>TEste</h1>
-    </div>
-  )
-}
-
-
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
+};
